f6cfc5ac572a9858befe787ae5e7b6facadc3874
wildlifehexagon <crossthebreeze@gmail.com>
2018-02-11 20:29:51 -0600

    convert actions to async/await

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -49,59 +49,81 @@ export function gameDeleted(gameId) {
   }
 }
 
-export function fetchGames() {
-  return dispatch => {
-    fetch("/api/games")
-      .then(res => res.json())
-      .then(data => dispatch(setGames(data.games)))
+export const fetchGames = () => {
+  return async dispatch => {
+    try {
+      const res = await fetch("/api/games")
+      const json = await res.json()
+      dispatch(setGames(json.games))
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
-export function fetchGame(id) {
-  return dispatch => {
-    fetch(`/api/games/${id}`)
-      .then(res => res.json())
-      .then(data => dispatch(gameFetched(data.game)))
+export const fetchGame = id => {
+  return async dispatch => {
+    try {
+      const res = await fetch(`/api/games/${id}`)
+      const json = await res.json()
+      dispatch(gameFetched(json.game))
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
-export function updateGame(data) {
-  return dispatch => {
-    return fetch(`/api/games/${data._id}`, {
-      method: "put",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(handleResponse)
-      .then(data => dispatch(gameUpdated(data.game)))
+export const updateGame = data => {
+  return async dispatch => {
+    try {
+      const res = await fetch(`/api/games/${data._id}`, {
+        method: "put",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      const json = await res.json()
+      handleResponse(res)
+      dispatch(gameUpdated(json.game))
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
-export function deleteGame(id) {
-  return dispatch => {
-    return fetch(`/api/games/${id}`, {
-      method: "delete",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(handleResponse)
-      .then(data => dispatch(gameDeleted(id)))
+export const deleteGame = id => {
+  return async dispatch => {
+    try {
+      const res = await fetch(`/api/games/${id}`, {
+        method: "delete",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      handleResponse(res)
+      dispatch(gameDeleted(id))
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
-export function saveGame(data) {
-  return dispatch => {
-    return fetch("/api/games", {
-      method: "post",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(handleResponse)
-      .then(data => dispatch(addGame(data.game)))
+export const saveGame = data => {
+  return async dispatch => {
+    try {
+      const res = await fetch("/api/games", {
+        method: "post",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      const json = await res.json()
+      handleResponse(res)
+      dispatch(addGame(json.game))
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
